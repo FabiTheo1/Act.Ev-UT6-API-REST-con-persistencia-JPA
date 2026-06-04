cdf3a0e7a1b5ed7ebe2073f1060b60ac33174e5f
Daniel Flores Medina <dd89jjktyh@privaterelay.appleid.com>
2026-06-04 17:24:47 +0100

    Creación de la lógica de búsqueda y filtros

--- src/main/java/com/biblioteca/biblioteca_api/repositories/LibroRepository.java
+++ src/main/java/com/biblioteca/biblioteca_api/repositories/LibroRepository.java
@@ -3,7 +3,11 @@ package com.biblioteca.biblioteca_api.repositories;
 import com.biblioteca.biblioteca_api.models.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
+    List<Libro> findByTituloContainingIgnoreCase(String titulo);
+
+    List<Libro> findByAutorNombreContainingIgnoreCase(String nombreAutor);
 }
