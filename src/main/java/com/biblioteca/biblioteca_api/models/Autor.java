e24f58056e10ab43cca4bc09eb149a94afcd0f87
Daniel Flores Medina <dd89jjktyh@privaterelay.appleid.com>
2026-06-04 17:17:54 +0100

    Corrección de la Clase Autor

--- src/main/java/com/biblioteca/biblioteca_api/models/Autor.java
+++ src/main/java/com/biblioteca/biblioteca_api/models/Autor.java
@@ -2,9 +2,16 @@ package com.biblioteca.biblioteca_api.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.List;
+import java.util.ArrayList;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Autor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,34 +19,7 @@ public class Autor {
 
     private String nombre;
 
-    // Relación OneToMany: Un autor tiene muchos libros
-    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
-    @JsonIgnore // Evita la recursión infinita en JSON
-    private List<Libro> libros;
-
-    // Getters, Setters y Constructores
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<Libro> getLibros() {
-        return libros;
-    }
-
-    public void setLibros(List<Libro> libros) {
-        this.libros = libros;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Libro> libros = new ArrayList<>();
 }
