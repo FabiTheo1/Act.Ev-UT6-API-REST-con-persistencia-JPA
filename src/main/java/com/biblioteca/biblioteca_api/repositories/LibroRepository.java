031fdc9922136f8e8c25b87d6cc8c1c9f7ebde76
FabianGOdev <fabianmaster531@gmail.com>
2026-06-06 13:02:06 +0100

    Delete src directory por duplicación y error en commit

--- src/main/java/com/biblioteca/biblioteca_api/repositories/LibroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biblioteca.biblioteca_api.repositories;
-
-import com.biblioteca.biblioteca_api.models.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibroRepository extends JpaRepository<Libro, Long> {
-}
